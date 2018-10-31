a7962d080c9d3b8c7cea1590db2c8a2b90da0213
chengsheng <chengsheng@qbb6.com>
2018-10-31 20:12:59 +0800

    fixbug

--- src/BuildJsonForYapi.java
+++ src/BuildJsonForYapi.java
@@ -208,6 +208,7 @@ public class BuildJsonForYapi extends AnAction {
         boolean beginIndexFlag = true;
         boolean endIndexFlag = true;
         do {
+            if(Strings.isNullOrEmpty(source)){break;}
             int beginIndex = source.indexOf(element) == 0 ? 1 : 0;
             int endIndex = source.lastIndexOf(element) + 1 == source.length() ? source.lastIndexOf(element) : source.length();
             source = source.substring(beginIndex, endIndex);
